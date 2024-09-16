c905f65ab9e0c32f6405b869992f596a97d83c23
Madalina Gomei <madalinagomei@gmail.com>
2024-09-16 21:09:55 +0200

    add years

--- src/components/StatisticsDashboard/StatisticsDashboard.jsx
+++ src/components/StatisticsDashboard/StatisticsDashboard.jsx
@@ -24,12 +24,23 @@ const StatisticsDashboard = () => {
   };
 
   const optionsYears = [
+    { value: 2021, label: '2024' },
     { value: 2023, label: '2023' },
     { value: 2022, label: '2022' },
     { value: 2021, label: '2021' },
+    { value: 2021, label: '2020' },
     { value: 2019, label: '2019' },
     { value: 2018, label: '2018' },
     { value: 2017, label: '2017' },
+    { value: 2017, label: '2016' },
+    { value: 2017, label: '2015' },
+    { value: 2017, label: '2014' },
+    { value: 2017, label: '2013' },
+    { value: 2017, label: '2012' },
+    { value: 2017, label: '2011' },
+    { value: 2017, label: '2010' },
+    { value: 2017, label: '2009' },
+    { value: 2017, label: '2008' },
   ];
   const optionsMonth = [
     { value: 1, label: 'January' },
